4c4fd0251d1cec42345ae55896b2afd0540927ef
philip <optionabc@gmail.com>
2019-11-15 21:56:45 -0500

    Signed-off-by: philip <optionabc@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-const Intersections = require('./intersections');
-const { loadCSVData } = require('./csv-loader');
+import Intersections from './intersections';
+import { loadCSVData } from './csv-loader';
 
 // For debugging purposes, all functions are available here...
-const Intersection = require('./intersection');
-const CameraViews = require('./camera-views');
-const Location = require('./location');
+import Intersection from './intersection';
+import CameraViews from './camera-views';
+import Location from './location';
 
 // TODO: You can try working with your functions/objects
 // here, to make development easier.  Eventually we'll
 // write our main program here.
 
 let csvData = loadCSVData();
-if(csvData) {
-  console.log(csvData);
+if (csvData) {
+  const intersections = new Intersections(csvData);
+  const streets = intersections.streets();
+  console.log(intersections);
+  console.log(streets);
+  var nav = document.querySelector('nav');
+  streets.forEach(function(street) {
+    let div = document.createElement('div');
+    console.log(div);
+    div.innerHTML = street;
+    console.log(street);
+    nav.appendChild(div);
+    div.classList.add('menu-item');
+  });
 }
